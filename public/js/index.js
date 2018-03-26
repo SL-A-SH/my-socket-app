8da0d8f2ef6bb51b3bc33124d073f66fd52927f5
Aqeeb <aqeeb.spider@gmail.com>
2018-03-26 16:48:47 +0530

    Add form for messages and shown incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,23 @@ socket.on('connect', () => {
 
 socket.on('newMessage', function(data) {
   console.log(data);
+  var li = $('<li></li>');
+  li.text(`${data.from}: ${data.text}`);
+
+  $('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User1',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
